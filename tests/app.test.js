9e3a9b74ec0d2ee86ba51903b916594ac5aad535
Suraj Prakash <surajprak201@gmail.com>
2026-04-29 21:25:09 +0530

    commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,36 +1,13 @@
-// import request from 'supertest'
-// import app from '../index.js'
 
-// describe('test the api',()=>{
+const request = require("supertest");
+const app = require("../index");
 
-//     test('test get method api',async() => {
-
-//         const res=await request(app).get('/')
-//         expect(res.statusCode).toBe(200)
-//         expect(res.text).toBe(`
-//             <h1>Welcome to the app</h1>
-//             <h2>Name: Suraj Prakash</h2>
-//             `)
-//     })
-
-//     test()
-// })
-
-import request from 'supertest'
-import app from '../index.js'
-
-describe('Test the API', () => {
-
-    test('GET / should return HTML response', async () => {
-
-        const res = await request(app).get('/')
-
-        expect(res.statusCode).toBe(200)
-
-        expect(res.text).toBe(
-`<h1>Welcome to the app</h1>
-<h2>Name: Suraj Prakash</h2>`
-        )
-    })
-
-})
+describe("test the api", () => {
+  test("test get method api", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.text.replace(/\s+/g, " ").trim()).toBe(
+      "<h1>Welcome to the app</h1> <h2>Name: suraj prakash</h2>",
+    );
+  });
+});
